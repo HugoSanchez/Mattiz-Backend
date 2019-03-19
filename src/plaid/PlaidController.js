783a270db2b29c4aaccc18f5e70078d5b7a705f9
Hugo Sanchez <hugosanchez@Hugos-MacBook.localdomain>
2019-03-19 18:50:53 +0100

    simplified get_access_token route

--- src/plaid/PlaidController.js
+++ src/plaid/PlaidController.js
@@ -30,35 +30,23 @@ const plaidClient = new plaid.Client (
     version
 );
 
-// STORE ACCESS TOKEN IN MEMORY 
-// Temporary, not in production.
-let ACCESS_TOKEN = null;
-let PUBLIC_TOKEN = null;
-let ITEM_ID = null;
-
             ///* Plaid Routes *///
 
-// POST: EXCHANGES PUBLICK TOKEN FOR ACCESS TOKEN.
+// POST: EXCHANGES PUBLIC TOKEN FOR ACCESS TOKEN.
 router.post('/get_access_token', function(req, res) {
     // First, get the public_token and return error if it doesn't exist. 
-    PUBLIC_TOKEN = req.body.public_token
-    if ( !PUBLIC_TOKEN ) return res.status(401).send({ error: true, message: 
+    if ( !req.body.public_token ) return res.status(401).send({ error: true, message: 
         'No token provided.' });
     
     // Call Plaid method to exchange token.
-    plaidClient.exchangePublicToken(PUBLIC_TOKEN, function(error, token) {
+    plaidClient.exchangePublicToken(req.body.public_token, function(error, token) {
         // If error, return error message. 
         if ( error ) return res.status(401).send({ error: true, message: 
         JSON.stringify(error) });
         
-        // If not, get the access token 
-        ACCESS_TOKEN = token.access_token;
-        ITEM_ID = token.item_id;
-        
-        // and send it as response.
+        // If not, send access_token and item_id as response.
         return res.status(200).send({ error: false, 
-        access_token: ACCESS_TOKEN, item_id: ITEM_ID });
-
+        access_token: token.access_token, item_id: token.item_id });
     });
 });
 
