76fe255223df13252a0402e709b5910f388ac481
Hugo Sanchez <hugosanchez@Hugos-MacBook.localdomain>
2019-03-23 17:10:59 +0100

    changed transactions route to accept multiple access token

--- src/plaid/PlaidController.js
+++ src/plaid/PlaidController.js
@@ -1,4 +1,4 @@
-     ///* Dependencies & Set Up */// 
+            ///* Dependencies & Set Up */// 
 
 // SERVER SIDE DEPENDENCIES
 const express = require('express');
@@ -34,11 +34,12 @@ const plaidClient = new plaid.Client (
 
 // POST: EXCHANGES PUBLIC TOKEN FOR ACCESS TOKEN.
 router.post('/get_access_token', function(req, res) {
+
     // First, get the public_token and return error if it doesn't exist. 
     if ( !req.body.public_token ) return res.status(401).send({ error: true, message: 
         'No token provided.' });
     
-    // Call Plaid method to exchange token.
+    // Then call Plaid method to exchange token.
     plaidClient.exchangePublicToken(req.body.public_token, function(error, token) {
         // If error, return error message. 
         if ( error ) return res.status(401).send({ error: true, message: 
@@ -52,6 +53,7 @@ router.post('/get_access_token', function(req, res) {
 
 // GET: RETURNS HIGH-LEVEL ACCOUNTS INFORMATION
 router.get('/accounts', function(req, res) {
+
     // Retrieve information by validating token in the request header.
     plaidClient.getBalance(req.headers.authorization, function(error, result) {
         // If error, return error message. 
@@ -67,31 +69,35 @@ router.get('/accounts', function(req, res) {
 
 // GET: RETURNS ALL TRANSACTIONS FOR A GIVEN ITEM
 router.get('/yearly_transactions', function(req, res) {
+
+    // Set end and start dates.
     let startDate = moment().subtract(365, 'days').format('YYYY-MM-DD');
     let endDate = moment().format('YYYY-MM-DD')
+
+    // Retrieve transaction information.
     plaidClient.getAllTransactions(req.headers.authorization, startDate, endDate, function(error, result) {
         // If error, return error message. 
         if ( error ) return res.status(401).send({ error: true, message: 
             JSON.stringify(error) });
 
-        // If not, send back response with accounts object plus aggregated balance.
-        return res.status(200).send({ error: false, response: result });
+        // If not, send back response with all transaction result.
+        return res.status(200).send({ error: false, data: result });
+    });
+});
+
+// GET: RETURNS TRANSACTIONS FOR THE LAST 90 DAYS 
+router.get('/last_90_days_transactions', async function(req, res){
+
+    // Set end and start dates.
+    let startDate = moment().subtract(90, 'days').format('YYYY-MM-DD');
+    let endDate = moment().format('YYYY-MM-DD')    
+
+    unifyTransactionsArray(req.body.accessTokenArray, startDate, endDate).then(result => {
+        unifiedTransactionArray = sortByDate(result[0].concat(result[1]))
+        console.log('unifiedTransactionArray: ', unifiedTransactionArray.slice(46, 52))
     })
-})
 
-router.get('/monthly_transactions', function(req, res){
-    let startDate = moment().subtract(30, 'days').format('YYYY-MM-DD');
-    let endDate = moment().format('YYYY-MM-DD')
-
-    plaidClient.getTransactions(req.headers.authorization, startDate, endDate, function(error, result) {
-        // If error, return error message. 
-        if ( error ) return res.status(401).send({ error: true, message: 
-            JSON.stringify(error) });
-
-        // If not, send back response with accounts object plus aggregated balance.
-        return res.status(200).send({ error: false, response: result });
-    })
-})
+});
 
 
             ///* Helper Methods *///
@@ -100,12 +106,43 @@ const computeTotalBalance = (accounts) => {
     // Initialize counter and set to 0.
     let totalBalance = 0;
     // Iterate over accounts array and for each one of them,
-    // sum the balance amount to the counter variable.
     accounts.forEach( account => {
+        // sum each it's balance to the counter variable.
         totalBalance = totalBalance + account.balances.current
-    })
+    });
     // Return the final counter.
     return totalBalance
+}
+
+const unifyTransactionsArray = async ( accessTokenArray, startDate, endDate ) => {
+    // Third promise 
+    return Promise.all(accessTokenArray.map(access_token => {
+        // Sencond Promise
+        return getTransactionsPromsise(access_token, startDate, endDate);
+    })).then(merge) 
+}
+
+// Second promise
+const getTransactionsPromsise = (access_token, startDate, endDate) => {
+    return new Promise( function(resolve, reject) {
+        plaidClient.getTransactions(access_token, startDate, endDate, async function(error, result) {
+            if (error) { reject(error) };
+            console.log('Deep: ', result.transactions.length)
+            resolve(result.transactions);
+        }
+    )}
+)}
+
+// Concat arrays.
+function merge(arrays) {
+    return [].concat(arrays)
+}
+
+// Sort array elements by date. 
+const sortByDate = (array) => {
+    return array.sort((a, b) => {
+        return new Date(b.date) - new Date(a.date)
+    })
 }
 
 // Export router 
