6b6d44bb9d914c5b5463ceb92edac56811ba1b6d
Hugo Sanchez <hugosanchez@Hugos-MacBook.local>
2019-07-24 14:34:45 +0200

    changed marketDataController to fetch up to date E

--- src/data/MarketDataController.js
+++ src/data/MarketDataController.js
@@ -18,14 +18,23 @@ router.use(bodyParser.json());
 ///* Data Routes *///
 
 
-// GET: .
-router.get('/get_historical_data', function(req, res) {
-    // First, get the public_token and return error if it doesn't exist. 
-    axios.get(
-        'https://api.nomics.com/v1/exchange-rates/history?key=' 
-        + config.nomics.key
-        + '&currency=ETH&start=2018-01-01T00%3A00%3A00Z&end=2019-04-18T00%3A00%3A00Z'
-        )
+// POST: GET ETH HISTORICAL DATA.
+router.post('/get_historical_data', function(req, res) {
+
+    // First, get current time up to minutes.
+    const currentTime = moment().format(); 
+    const dayAndHour = currentTime.slice(0, 13)
+    const minutes = currentTime.slice(14, 16)
+
+    // Then subtract based on timeframe.
+    const startTime = start(req.body.timeframe)
+    
+    // Call Nomics API.
+    axios.get('https://api.nomics.com/v1/exchange-rates/history?key=' 
+    + config.nomics.key + '&currency=ETH&'
+    + 'start='+ startTime + 'T00%3A00%3A00Z&'
+    + 'end=' + dayAndHour + '%3A' + minutes + '%3A00Z')
+        // Then send status 200 with rates array.
         .then( async response => {
             res.status(200).send({ 
                 error: false, 
@@ -34,6 +43,22 @@ router.get('/get_historical_data', function(req, res) {
         })
         .catch(err => console.log(err))
 });
+
+// HELPER METHODS // 
+const start = timeframe => {
+    switch (timeframe) {
+        case 'week':
+            return moment().subtract(7, 'days').format().slice(0, 10);
+        case 'month':
+            return moment().subtract(1, 'month').format().slice(0, 10);
+        case 'quarter':
+            return moment().subtract(3, 'month').format().slice(0, 10);
+        case 'year':
+            return moment().subtract(12, 'month').format().slice(0, 10);
+        default:
+            return moment().subtract(3, 'month').format().slice(0, 10);
+    }
+}
 
 // Export router 
 module.exports = router; 
