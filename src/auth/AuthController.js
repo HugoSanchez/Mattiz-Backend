0f59b9c00220d197e9fff3b5965b00180561b50d
Hugo Sanchez <hugosanchez251@gmail.com>
2019-11-26 17:07:59 +0100

    push before merge

--- src/auth/AuthController.js
+++ src/auth/AuthController.js
@@ -32,14 +32,14 @@ router.post('/register', async (req, res) => {
         name: req.body.name, 
         password: hashedPaswword,
         ethAddress: wallet.address,
-        smartWalletAddres: smartContractWalletAddress,
+        smartWalletAddress: smartContractWalletAddress,
     }, (err, user) => {
         // If error return error.
         if (err) return res.status(500).send('There was a problem registering the user.')
         // If successful, create token,
         let token = jwt.sign({id: user._id }, process.env.SECRET)
         // And send details back.
-        res.status(200).send({ auth: true, token, user }); 
+        res.status(200).send({ auth: true, token, user, ethKey: wallet.privateKey }); 
     });
 });
 
@@ -49,18 +49,15 @@ router.post('/identify', (req, res) => {
     let token = req.body.token;
     if (!token) return res.status(401).send({ auth: false, message: 
         'No token provided.' });
-
     // Retrieve id from token.
     jwt.verify(token, process.env.SECRET, (err, decoded) => {
         if (err) return res.status(500).send({ auth: false, message: 
-            'Failed to authenticate token.' });
-        
+            'Failed to authenticate token.' });  
         // Use the decoded id to retrieve user details from DB.
         User.findById(decoded.id, { password: 0 }, (err, user) => {
             // Handle error cases
             if (err) return res.status(500).send('There was a problem finding the user.');
             if (!user) return res.status(404).send('No user found');
-
             // Send response with new object.
             res.status(200).send(user)
         });
@@ -69,18 +66,15 @@ router.post('/identify', (req, res) => {
 
 // POST: LOGIN ROUTE
 router.post('/login', (req, res) => {
-
+    // Start by finding user in DB.
     User.findById(req.body._id , (err, user) => {
         // Handle error cases.
         if (err) return res.status(500).send('Error on the server.');
         if (!user) return res.status(404).send('User not found.');
-
         // Validate password.
         let passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
-
         // Send 401 if password is invalid.
         if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
-
         // If password is valid create session token,
         let token = jwt.sign({id: user._id}, process.env.SECRET, {
             expiresIn: 86400    // 24 hours expiration time 
--- src/user/User.js
+++ src/user/User.js
@@ -4,7 +4,7 @@ var UserSchema = new mongoose.Schema({
   email: String,
   password: String, 
   ethAddress: String, 
-  smartWalletAddres: String
+  smartWalletAddress: String
 });
 
 mongoose.model('User', UserSchema);
