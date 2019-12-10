ea738f2464282893a25837808932bdf34f986c1e
HugoSanchez <hugosanchez251@gmail.com>
2019-12-10 15:10:46 +0100

    small changes to not need ./config folder anymore

--- db.js
+++ db.js
@@ -1,7 +1,10 @@
-var mongoose = require('mongoose');
-var config = require('./config')
+require('dotenv').config();
 
-mongoose.connect(config.dbUrl);
+const mongoose = require('mongoose');
+
+const db = process.env.DB_URL;
+
+mongoose.connect(db);
 
 mongoose.connection.on("open", function(ref) {
     console.log("\n Connected to mongo server.");
